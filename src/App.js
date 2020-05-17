6e52872908b87e96b2b66faca83b62f925e3b07b
Kasun Peiris <me@kasun.io>
2020-05-17 15:57:07 +0530

    Initial commi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello friend!</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga autem excepturi, asperiores beatae inventore officia ullam adipisci commodi? Rem repellat veniam expedita maxime itaque provident deserunt voluptatibus aut a quaerat.
+      </p>
     </div>
   );
 }
